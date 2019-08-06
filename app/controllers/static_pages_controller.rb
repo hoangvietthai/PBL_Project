92d4c2e9a0b0b405a39d31e067a9783b4dc0585b
hosi taichi <minmin55gg@gmail.com>
2019-08-07 00:39:37 +0700

    学生検索機能（ロジックのみ）and 検索リファクタ

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,18 +1,43 @@
 class StaticPagesController < ApplicationController
   def home
+    # 企業
     if (current_user!=nil && current_user.type_user==2)
        @jobs = Job.where(company_id: current_user.id)
+       @conditionOptions = {"title": "仕事の名前", "deadline": "締切", "salary": "給料", "typejob_id": "種類", "address": "場所"}
+      #POST処理
+      if request.post?
+        if params[:searchText].present? and params[:condition].present?
+          @searchResult = @jobs.where(["#{params[:searchText]} like ?" , "%#{params[:condition]}%"])
+          @searchMessage = "検索条件: \"" << @conditionOptions[:"#{params[:condition]}"] << "\" で \"#{params[:searchText]}\" を検索"
+        else
+          @searchResult = Job.where(company_id: current_user.id)
+          @searchMessage = "登録した仕事を全件表示"
+        end
+      end
     end
+    # 学生
     if (current_user!=nil && current_user.type_user==1)
       @a = Typejob.where(name_job: current_user.major).first
       @jobs = Job.where(typejob_id: @a.id)
       @user = User.all
-    end
+      @conditionOptions = {"title": "仕事の名前", "deadline": "締切", "salary": "給料",  "address": "場所"}
+      #POST処理
+      if request.post?
+        if params[:searchText].present? and params[:condition].present?
+          @searchResult = @jobs.where(["#{params[:searchText]} like ?", "%#{params[:condition]}%"])
+          @searchMessage = "検索条件: \"" << @conditionOptions[:"#{params[:condition]}"] << "\" で \"#{params[:searchText]}\" を検索"
+        else
+          @searchResult = Job.where(company_id: current_user.id)
+          @searchMessage = ""
+        end
+      end
+      end
     if (current_user==nil)
       @jobs = Job.all
       @user = User.all
+      @conditionOptions = {"title": "仕事の名前、概要", "deadline": "締切", "salary": "給料", "typejob_id": "種類", "address": "場所"}
     end
-    @conditionOptions = {"title": "仕事の名前、概要", "deadline": "締切", "salary": "給料", "typejob_id": "種類", "address": "場所"}
+
   end
 
   def help
@@ -21,23 +46,4 @@ class StaticPagesController < ApplicationController
   def about
   end
 
-  # 企業の検索機能
-  def searchOwnJobs
-    if current_user!=nil
-      @jobs = Job.where(company_id: current_user.id)
-    else
-      @jobs = Job.all
-      @user = User.all
-    end
-    @conditionOptions = {"title": "仕事の名前、概要", "deadline": "締切", "salary": "給料", "typejob_id": "種類", "address": "場所"}
-    # ここまで home と同じ。改善できそう
-    if params[:searchText].present? and params[:condition].present?
-      @searchResult = Job.where(["company_id = ? and #{params[:searchText]} like ?", current_user.id, "%#{params[:condition]}%"])
-      @searchMessage = "検索条件: \"" << @conditionOptions[:"#{params[:condition]}"] << "\" で \"#{params[:searchText]}\" を検索"
-    else
-      @searchResult = Job.where(company_id: current_user.id)
-      @searchMessage = "登録した仕事を全件表示"
-    end
-    render 'home'
-  end
 end
