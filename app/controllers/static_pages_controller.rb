44b779f430fc084a72051310e3ea299854edeb1b
hoang.viet.thai <SUN-ASTERISK\hoang.viet.thai@b121982-lt.sun-asterisk.com>
2019-08-06 19:55:47 +0700

    sua loi hien thi trang top

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,10 +1,16 @@
 class StaticPagesController < ApplicationController
   def home
-    if current_user!=nil
+    if (current_user!=nil && current_user.type_user==2)
        @jobs = Job.where(company_id: current_user.id)
-    else
-       @jobs = Job.all
-       @user = User.all
+    end
+    if (current_user!=nil && current_user.type_user==1)
+      @a = Typejob.where(name_job: current_user.major).first
+      @jobs = Job.where(typejob_id: @a.id)
+      @user = User.all
+    end
+    if (current_user==nil)
+      @jobs = Job.all
+      @user = User.all
     end
   end
 
