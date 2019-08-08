9b54901164c256a75cda552acb914dd58a060a63
hosi taichi <minmin55gg@gmail.com>
2019-08-08 13:26:14 +0700

    admin search

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -29,7 +29,16 @@ class AdminsController < ApplicationController
   def show
     @user = User.all
     @job = Job.all
-    @a = User.new
+    @conditionOptionsUser = {"name": "学生、企業の名前", "mail": "メールアドレス", "university": "大学", "major": "種類", "address": "住所"}
+    @conditionOptionsJob  ={"title": "仕事の名前", "deadline": "締切", "salary": "給料", "typejob_id": "種類", "address": "場所"}
+    if params[:searchUser].present? and params[:searchTextUser].present? and params[:conditionUser].present?
+      @user = @user.where(["#{params[:conditionUser]} like ?" , "%#{params[:searchTextUser]}%"])
+      @searchMessageUser = "条件: \"" << @conditionOptionsUser[:"#{params[:conditionUser]}"] << "\" で \"#{params[:searchTextUser]}\" を検索"
+    end
+    if params[:searchJob].present? and params[:searchTextJob].present? and params[:conditionJob].present?
+      @job = @job.where(["#{params[:conditionJob]} like ?" , "%#{params[:searchTextJob]}%"])
+      @searchMessageJob = "条件: \"" << @conditionOptionsJob[:"#{params[:conditionJob]}"] << "\" で \"#{params[:searchTextJob]}\" を検索"
+    end
     render :show
   end
 
